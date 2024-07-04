2fb0b609d3198edf0089c5078b697f00b785dd83
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-04 17:50:26 -0300

    refactor: mcv

---
+++ Ambiente Node/db.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg')
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log("a conexão funcionou")
+    })
+    .catch((err) => {
+        console.log("erro ao conectar na db")
+    })
+}
+
+module.exports = connectDB
--- Ambiente Node/index.js
+++ Ambiente Node/index.js
@@ -1,31 +1,11 @@
 const express = require('express')
-const { Client } = require('pg')
 require("dotenv").config()
-
-const client = new Client({
-    host: process.env.host,
-    port: process.env.port,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
-})
+const connectDb = require("./db")
 
 const app = express()
-
-const connectDB = async () => {
-    client
-    .connect()
-    .then(() => {
-        console.log("a conexão funcionou")
-    })
-    .catch((err) => {
-        console.log("erro ao conectar na db")
-    })
-}
-
+connectDb()
 app.get('/test-api', function (req, res) {
     res.send('{}')
 })
 
 app.listen(3000)
-connectDB()
